b47e71c415e61dfe45fb544d9f609f3bd6d7cea6
SkyNet <nme312.noreply.github.com>
2018-04-28 13:21:43 -0500

    added form that turns user input into new gif button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,60 +8,71 @@ $(document).ready(function () {
         "movies",
         "the office"
     ];
+
     var API_KEY = "h70cZoZOFSjjIar9455xHj8U1Eg9nal3";
-    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&limit=10&q=";
-
-    var search = $("#search");
-    topics.push(search);
-
+    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&limit=10&rating=&q=";
 
     for (i = 0; i < topics.length; i++) {
+
         var button = $("<button>");
         button.text(topics[i]);
-
+        button.addClass("btn btn-info topic");
         $("#buttons").append(button);
-
-        button.addClass("btn btn-info border");
-
-        button.on("click", function () {
-            console.log(this);
-            $("#gifs").empty();
-            $.ajax({
-                method: "GET",
-                url: requestUrl + $(this).text()
-            }).then(function (response) {
-
-                console.log(response.data.limit);
-                for (var i = 0; i < response.data.length; i++) {
-                    var img = $("<img>");
-                    img.attr("src", response.data[i].images.downsized.url);
-                    img.attr("data-animated", response.data[i].images.downsized.url);
-                    img.attr("data-still", response.data[i].images.downsized_still.url);
-                    img.attr("data-state", "animated");
-
-                    img.on("click", function () {
-                        var state = $(this).attr("data-state");
-                        if (state === "animated") {
-                            $(this).attr("data-state", "still");
-                            $(this).attr("src", $(this).attr("data-still"));
-                        } else {
-                            $(this).attr("data-state", "animated");
-                            $(this).attr("src", $(this).attr("data-animated"));
-                        }
-                    });
-                    $("#gifs").append(img);
-                }
-            });
-
-
-        });
-
     }
-    // console.log(button);
-    console.log("lets see if the rename worked");
 
+    function createImage(response, i) {
+        var img = $("<img class='giphy-img'>");
+        var rating = $("#rating");
+        //set the src of the element
+        img.attr("src", response.data[i].images.downsized_still.url);
+        img.attr("data-animated", response.data[i].images.downsized.url);
+        img.attr("data-still", response.data[i].images.downsized_still.url);
+        img.attr("data-state", "still");
 
+        var gifCard = $("<div class='gif-card'>");
+        var pRating = $("<p>");
 
+        gifCard.append(img);
+        pRating.append("Rating: " + response.data[i].rating);
+        gifCard.append(pRating);
 
+        return gifCard;
+    }
 
+    $(document).on("click", ".topic", function () {
+        // console.log(this);
+        $("#gifs").empty();
+        $.ajax({
+            method: "GET",
+            url: requestUrl + $(this).text()
+        }).then(function (response) {
+            // console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+                var img = createImage(response, i);
+                // console.log(response.data[i].rating);
+                $("#gifs").append(img);
+            }
+        });
+    });
+
+    $(document).on("click", ".giphy-img", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("data-state", "animated");
+            $(this).attr("src", $(this).attr("data-animated"));
+        } else {
+            $(this).attr("data-state", "still");
+            $(this).attr("src", $(this).attr("data-still"));
+        }
+    });
+    var searchButton = $("#search-button");
+    searchButton.addClass("btn btn-info");
+
+    $(searchButton).on("click", function () {
+        var query = $("#query").val();
+        var button = $("<button>");
+        button.text(query);
+        button.addClass("btn btn-info topic");
+        $("#buttons").append(button);
+    });
 });
